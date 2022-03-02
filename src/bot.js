d7346af3ba2d368073ecec6f1242fe27fafc46ce
ApteryxXYZ <apteryx.xyz@gmail.com>
2022-03-02 20:50:06 +1300

    🐛 Fix env system

--- src/bot.js
+++ src/bot.js
@@ -1,11 +1,10 @@
-const config = require('../config.json');
-global.ENV = process.env.ENV = (process.env.ENV || config.env);
-require('dotenv').config({ path: `.env.${ENV}` });
+require('dotenv').config();
+global.ENV = process.env.ENV;
 
 const { Intents } = require('discord.js');
 const intents = new Intents(Object.values(Intents.FLAGS));
 const Client = require('./structures/Client');
 const client = new Client({ intents });
-client.config = config;
+client.config = require('../config.json');
 require('discord-modals')(client);
 module.exports = client.connect(process.env.DISCORD_TOKEN);

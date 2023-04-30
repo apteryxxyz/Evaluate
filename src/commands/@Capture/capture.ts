8a2a451ddcad0627edc711f8ee0fa16bc7b17194
Apteryx <apteryx.xyz@gmail.com>
2023-04-30 15:27:07 +1200

    Add mode to capture command

--- src/commands/@Capture/capture.ts
+++ src/commands/@Capture/capture.ts
@@ -8,6 +8,7 @@ import type { Action } from 'maclary';
 import { Command } from 'maclary';
 import { buildRenderAttachmentPayload } from '&factories/renderer';
 import { IncrementCommandCount } from '&preconditions/IncrementCommandCount';
+import { Renderer } from '&services/Renderer';
 
 export class Capture extends Command<
     Command.Type.ChatInput,
@@ -29,6 +30,13 @@ export class Capture extends Command<
                         'The code to convert into an image, emitting this will cause a modal to appear.',
                     maxLength: 900,
                 },
+                {
+                    type: Command.OptionType.String,
+                    name: 'mode',
+                    description: 'Dark or light mode, defaults to light.',
+                    choices: Object.entries(Renderer.Mode) //
+                        .map(([name, value]) => ({ name, value })),
+                },
             ],
         });
     }
@@ -36,6 +44,9 @@ export class Capture extends Command<
     public override async onSlash(input: Command.ChatInput) {
         let action: Action.AnyInteraction | Command.AnyInteraction = input;
         let code = input.options.getString('code');
+        const mode = (input.options.getString('mode') ?? undefined) as
+            | Renderer.Mode
+            | undefined;
 
         if (!code) {
             await input.showModal(this._buildModal());
@@ -51,7 +62,7 @@ export class Capture extends Command<
 
         await action.deferReply();
         const image = await this.container.renderer.createRender(
-            { code },
+            { code, mode },
             input.user.id
         );
 

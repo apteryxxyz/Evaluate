0d31a4389f33aa5a8aa3ecfe51153e06eb19a3d7
Apteryx <apteryx.xyz@gmail.com>
2024-01-11 14:14:33 +1300

    Attempt three to fix for production only error

--- apps/website/src/app/api/send/route.ts
+++ apps/website/src/app/api/send/route.ts
@@ -7,33 +7,24 @@ export async function POST(request: NextRequest) {
   )
     return new NextResponse(null, { status: 403 });
 
-  const response = await fetch(
-    'https://us.umami.is/api/send', //
-    { method: 'POST', headers: request.headers, body: request.body },
-  );
-  void response;
+  const response = await fetch('https://us.umami.is/api/send', {
+    method: 'POST',
+    headers: new Headers(request.headers),
+    body: await request.text(),
+  });
 
-  return new NextResponse(null);
-
-  // const headers = new Headers(request.headers);
-  // const body = await request.text();
-  // const response = await fetch(
-  //   'https://us.umami.is/api/send', //
-  //   { method: 'POST', headers, body },
-  // );
-
-  // return new NextResponse(null, {
-  //   status: response.status,
-  //   headers: response.headers,
-  // });
+  return new NextResponse(await response.text(), {
+    status: response.status,
+    headers: new Headers(response.headers),
+  });
 }
 
-// export async function OPTIONS() {
-//   return new NextResponse(null, {
-//     headers: {
-//       'access-control-allow-origin': '*',
-//       'access-control-allow-methods': 'POST, OPTIONS',
-//       'access-control-allow-headers': 'Content-Type, X-Umami-Id',
-//     },
-//   });
-// }
+export async function OPTIONS() {
+  return new NextResponse(null, {
+    headers: {
+      'access-control-allow-origin': '*',
+      'access-control-allow-methods': 'POST, OPTIONS',
+      'access-control-allow-headers': 'Content-Type, X-Umami-Id',
+    },
+  });
+}

c5af7716c906578d2e6aef27f6fb640eccdb4fef
Apteryx <apteryx.xyz@gmail.com>
2024-02-06 00:24:05 +1300

    Refactor typography components

--- apps/website/src/app/(policies)/policies/privacy-policy/page.tsx
+++ apps/website/src/app/(policies)/policies/privacy-policy/page.tsx
@@ -1,5 +1,3 @@
-import { Heading2 } from '~/components/typography/heading';
-import { Anchor, Paragraph } from '~/components/typography/text';
 import { formatDate } from '~/utilities/format-helpers';
 
 export default function PrivacyPolicyPage() {
@@ -25,47 +23,56 @@ export default function PrivacyPolicyPage() {
       </div>
 
       <div>
-        <Heading2>Introduction</Heading2>
-        <Paragraph>
+        <h2 className="mt-10 scroll-m-20 border-b pb-1 text-3xl font-semibold tracking-tight first:mt-0">
+          Introduction
+        </h2>
+        <p className="leading-7 [&:not(:first-child)]:mt-6">
           The privacy of our visitors is a top priority for Apteryx Software
           ("we", "our", "us") and, by extension, Evaluate ("service") and its
           products. The types of information that Evaluate gathers and records,
           as well as how we utilise it, are detailed in this privacy policy
           document.
-        </Paragraph>
+        </p>
 
-        <Heading2>Data Collection and Usage</Heading2>
-        <Paragraph>
+        <h2 className="mt-10 scroll-m-20 border-b pb-1 text-3xl font-semibold tracking-tight first:mt-0">
+          Data Collection and Usage
+        </h2>
+        <p className="leading-7 [&:not(:first-child)]:mt-6">
           Our service does not collect any information itself. We use{' '}
-          <Anchor
+          <a
+            className="font-medium underline underline-offset-4"
             href="https://posthog.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
             PostHog
-          </Anchor>{' '}
+          </a>{' '}
           analytics to collect data about user behaviour, custom events capture,
           session recordings, feature flags, and more. This data is used to
           analyse traffic and usage of our all of our services. This allows us
           to provide a better user experience for our visitors.
-        </Paragraph>
+        </p>
 
-        <Heading2>Policy Acceptance</Heading2>
-        <Paragraph>
+        <h2 className="mt-10 scroll-m-20 border-b pb-1 text-3xl font-semibold tracking-tight first:mt-0">
+          Policy Acceptance
+        </h2>
+        <p className="leading-7 [&:not(:first-child)]:mt-6">
           Continued use of our service signifies your acceptance of this policy.
           If you do not accept the policy then please do not use our services.
           When registering we will further request your explicit acceptance of
           the privacy policy.
-        </Paragraph>
+        </p>
 
-        <Heading2>Policy Changes</Heading2>
-        <Paragraph>
+        <h2 className="mt-10 scroll-m-20 border-b pb-1 text-3xl font-semibold tracking-tight first:mt-0">
+          Policy Changes
+        </h2>
+        <p className="leading-7 [&:not(:first-child)]:mt-6">
           We may update this privacy policy from time to time. We encourage you
           to review this privacy policy periodically to stay informed about how
           we are helping to protect the personal information we collect. Your
           continued use of this site after any change in this privacy policy
           will constitute your acceptance of such change.
-        </Paragraph>
+        </p>
       </div>
     </>
   );

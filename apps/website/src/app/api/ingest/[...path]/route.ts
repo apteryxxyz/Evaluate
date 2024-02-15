55b3d3c95d899003886a2091f4cba5c0a1916722
Apteryx <apteryx.xyz@gmail.com>
2024-02-16 01:22:23 +1300

    Simplify route endpoint

--- apps/website/src/app/api/ingest/[...path]/route.ts
+++ apps/website/src/app/api/ingest/[...path]/route.ts
@@ -1,25 +1,13 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 
-async function handler(request: NextRequest) {
+function handler(request: NextRequest) {
   const pathname = request.nextUrl.pathname.replace('/api/ingest', '');
   const url = new URL(pathname, 'https://app.posthog.com/');
   url.search = request.nextUrl.searchParams.toString();
 
-  try {
-    const response = await fetch(url.toString(), {
-      method: request.method,
-      headers: request.headers,
-      body: request.body ? await request.arrayBuffer() : undefined,
-    });
-
-    return new NextResponse(await response.arrayBuffer(), {
-      status: response.status,
-      headers: response.headers,
-    });
-  } catch (error) {
-    console.error('[api/ingest]', error);
-    return new NextResponse(null, { status: 500 });
-  }
+  const originRequest = new NextRequest(request);
+  originRequest.headers.delete('cookie');
+  return fetch(url.toString(), originRequest);
 }
 
 export const GET = handler;

48af6a26c03334b6fa7ed981d5225f87cde0f184
Apteryx <apteryx.xyz@gmail.com>
2024-08-28 23:52:12 +1200

    Disable AI services

--- packages/server/src/routers/ai/router.ts
+++ packages/server/src/routers/ai/router.ts
@@ -1,42 +1,44 @@
 import { GenerateCodeOptions } from '@evaluate/types';
 import { aiProcedure, createTRPCRouter } from '~/services/trpc';
-import { runModel } from './run-model';
 
 export const aiRouter = createTRPCRouter({
   /**
    * Generate code based on the given instructions and file context.
    * @param input - The file and instructions to generate code
    */
-  generateCode: aiProcedure
-    .input(GenerateCodeOptions)
-    .mutation(async ({ input: { file, instructions } }) => {
-      return runModel('@cf/meta/llama-3-8b-instruct', [
-        {
-          role: 'system',
-          content:
-            'You are an expert coding assistant. You read code from a file, and you suggest new code to add to the file. You may be given instructions on what to generate, which you should follow. You should generate code that is CORRECT, efficient, and follows best practices. You may generate multiple lines of code if necessary. When you generate code, you should ONLY return the code, and nothing else. You MUST NOT include backticks in the code you generate.',
-        },
-        {
-          role: 'user',
-          content: `The file is called ${file.name}.`,
-        },
-        {
-          role: 'user',
-          content: `Here are my instructions on what to generate: ${instructions}.`,
-        },
-        {
-          role: 'user',
-          content: `Suggest me code to insert at line ${file.line} in my file. Give only the code, and NOTHING else. DO NOT include backticks in your response. My code file content is as follows
+  generateCode: aiProcedure.input(GenerateCodeOptions).mutation(async () => {
+    return {
+      response: 'AI services are currently disabled',
+      success: false,
+    };
 
-${file.content}`,
-        },
-      ]).then((response) => {
-        return {
-          response: response.result.response,
-          success: response.success,
-        };
-      });
-    }),
+    //       return runModel('@cf/meta/llama-3-8b-instruct', [
+    //         {
+    //           role: 'system',
+    //           content:
+    //             'You are an expert coding assistant. You read code from a file, and you suggest new code to add to the file. You may be given instructions on what to generate, which you should follow. You should generate code that is CORRECT, efficient, and follows best practices. You may generate multiple lines of code if necessary. When you generate code, you should ONLY return the code, and nothing else. You MUST NOT include backticks in the code you generate.',
+    //         },
+    //         {
+    //           role: 'user',
+    //           content: `The file is called ${file.name}.`,
+    //         },
+    //         {
+    //           role: 'user',
+    //           content: `Here are my instructions on what to generate: ${instructions}.`,
+    //         },
+    //         {
+    //           role: 'user',
+    //           content: `Suggest me code to insert at line ${file.line} in my file. Give only the code, and NOTHING else. DO NOT include backticks in your response. My code file content is as follows
+
+    // ${file.content}`,
+    //         },
+    //       ]).then((response) => {
+    //         return {
+    //           response: response.result.response,
+    //           success: response.success,
+    //         };
+    //       });
+  }),
 
   //   identifyCode: aiProcedure
   //     .input(z.string())

4caf39d9382ef58cba7c28c83a25284c0eaf8c2b
Apteryx <apteryx.xyz@gmail.com>
2024-12-11 18:24:06 +1300

    Remove explorer context menus, add replacement buttons

--- apps/website/src/components/explorer/file/item.tsx
+++ apps/website/src/components/explorer/file/item.tsx
@@ -1,15 +1,16 @@
 'use client';
 
 import { Button } from '@evaluate/react/components/button';
+import { cn } from '@evaluate/react/utilities/class-name';
+import { TextCursorInputIcon, Trash2Icon } from 'lucide-react';
 import type { File } from 'virtual-file-explorer-backend';
-import { ContextMenuWrapper } from '~/components/context-menu-wrapper';
 import { MaterialIcon } from '~/components/material-icon';
+import { useIsMobile } from '~/hooks/use-is-mobile';
 import { ExplorerItemName } from '../name';
 import { useWatch } from '../use';
 import {
   useClickable,
   useDeleteable,
-  useDownloadable,
   useDraggable,
   useNameable,
 } from './hooks';
@@ -23,36 +24,54 @@ namespace ExplorerFileItem {
 export function ExplorerFileItem({ file, meta }: ExplorerFileItem.Props) {
   useWatch(file, ['selected']);
 
+  const isMobile = useIsMobile();
   const { draggableRef } = useDraggable(file);
   const { handleClick } = useClickable(file);
   const { naming, setNaming, handleRenameClick } = useNameable(file);
   const { handleDeleteClick } = useDeleteable(file);
-  const { handleDownloadClick } = useDownloadable(file);
 
   return (
-    <ContextMenuWrapper
-      items={
-        meta
-          ? []
-          : [
-              { label: 'Rename File', action: handleRenameClick },
-              { label: 'Delete File', action: handleDeleteClick },
-              null,
-              { label: 'Download File', action: handleDownloadClick },
-            ]
-      }
+    <Button
+      ref={draggableRef}
+      variant={file.selected ? 'secondary' : 'ghost'}
+      className="group relative flex h-auto w-full justify-start rounded-none p-0"
+      style={{ paddingLeft: `${6 + (file.ancestors.length - 1) * 6}px` }}
+      onClick={handleClick}
+      data-ignore-blur
     >
-      <Button
-        ref={draggableRef}
-        variant={file.selected ? 'secondary' : 'ghost'}
-        className="flex h-auto w-full justify-start rounded-none p-0"
-        style={{ paddingLeft: `${6 + (file.ancestors.length - 1) * 6}px` }}
-        onClick={handleClick}
-        data-ignore-blur
-      >
-        <MaterialIcon type="file" name={file.name} naming={naming} />
-        <ExplorerItemName item={file} naming={naming} setNaming={setNaming} />
-      </Button>
-    </ContextMenuWrapper>
+      <MaterialIcon type="file" name={file.name} naming={naming} />
+      <ExplorerItemName item={file} naming={naming} setNaming={setNaming} />
+
+      {!meta && (
+        <div
+          className={cn(
+            'absolute top-[20%] right-0',
+            isMobile ? 'block' : 'hidden group-hover:block',
+          )}
+        >
+          <Button
+            type="button"
+            size="icon"
+            variant={null}
+            className="h-auto w-auto p-0"
+            onClick={handleRenameClick}
+          >
+            <TextCursorInputIcon size={16} />
+            <span className="sr-only">Rename File</span>
+          </Button>
+
+          <Button
+            type="button"
+            size="icon"
+            variant={null}
+            className="h-auto w-auto p-0"
+            onClick={handleDeleteClick}
+          >
+            <Trash2Icon size={16} />
+            <span className="sr-only">Delete File</span>
+          </Button>
+        </div>
+      )}
+    </Button>
   );
 }
--- apps/website/src/components/explorer/index.tsx
+++ apps/website/src/components/explorer/index.tsx
@@ -4,9 +4,12 @@ import { Button } from '@evaluate/react/components/button';
 import { ScrollArea } from '@evaluate/react/components/scroll-area';
 import { Separator } from '@evaluate/react/components/separator';
 import { cn } from '@evaluate/react/utilities/class-name';
-import { FilePlusIcon, FolderPlusIcon } from 'lucide-react';
+import {
+  FilePlusIcon,
+  FolderPlusIcon,
+  HardDriveDownloadIcon,
+} from 'lucide-react';
 import type { File } from 'virtual-file-explorer-backend';
-import { ContextMenuWrapper } from '../context-menu-wrapper';
 import { ExplorerFileItem } from './file/item';
 import { ExplorerFolderChildren } from './folder/children';
 import {
@@ -14,7 +17,6 @@ import {
   useClickable,
   useDownloadable,
   useDropzone,
-  useUploadable,
 } from './folder/hooks';
 import { useExplorer, useWatch } from './use';
 
@@ -29,81 +31,74 @@ export function Explorer() {
   const { isOver, dropzoneRef } = useDropzone(explorer);
   const { handleClick } = useClickable(explorer);
   const { handleDownloadClick } = useDownloadable(explorer);
-  const { handleUploadClick, inputRef } = useUploadable(explorer);
-  const {
-    handleNewFileToolbarClick,
-    handleNewFolderToolbarClick,
-    handleNewFileContextClick,
-    handleNewFolderContextClick,
-  } = useChildren(explorer);
+  const { handleNewFileClick, handleNewFolderClick } = useChildren(explorer);
 
   return (
-    <ContextMenuWrapper>
-      <section className="h-full w-full">
-        <div className="flex h-10 items-center gap-1 border-b px-3 py-1">
-          <span className="mr-auto font-medium text-sm">Explorer</span>
+    <section className="h-full w-full">
+      <div className="flex h-10 items-center gap-1 border-b px-3 py-1">
+        <span className="mr-auto font-medium text-sm">Explorer</span>
 
-          <input ref={inputRef} type="file" className="hidden" multiple />
+        {/* TODO: Upload button? Would likely need a confirm dialog as this will overwrite the current content */}
 
-          <Button
-            title="New File"
-            size="icon"
-            variant="ghost"
-            className="size-auto rounded-full"
-            onClick={handleNewFileToolbarClick}
-          >
-            <FilePlusIcon className="size-4" />
-            <span className="sr-only">New File</span>
-          </Button>
+        <Button
+          title="Download As Zip"
+          size="icon"
+          variant="ghost"
+          className="size-auto rounded-full"
+          onClick={handleDownloadClick}
+        >
+          <HardDriveDownloadIcon size={16} />
+          <span className="sr-only">Download As Zip</span>
+        </Button>
 
-          <Button
-            title="New Folder"
-            size="icon"
-            variant="ghost"
-            className="size-auto rounded-full"
-            onClick={handleNewFolderToolbarClick}
-          >
-            <FolderPlusIcon className="size-4" />
-            <span className="sr-only">New Folder</span>
-          </Button>
-        </div>
+        <Button
+          title="New File"
+          size="icon"
+          variant="ghost"
+          className="size-auto rounded-full"
+          onClick={handleNewFileClick}
+        >
+          <FilePlusIcon className="size-4" />
+          <span className="sr-only">New File</span>
+        </Button>
 
-        <div className="h-[calc(100%_-_40px)]">
-          {args && <ExplorerFileItem file={args} meta />}
-          {input && <ExplorerFileItem file={input} meta />}
-          <Separator />
+        <Button
+          title="New Folder"
+          size="icon"
+          variant="ghost"
+          className="size-auto rounded-full"
+          onClick={handleNewFolderClick}
+        >
+          <FolderPlusIcon className="size-4" />
+          <span className="sr-only">New Folder</span>
+        </Button>
+      </div>
 
-          <ContextMenuWrapper
-            items={[
-              { label: 'New File', action: handleNewFileContextClick },
-              { label: 'New Folder', action: handleNewFolderContextClick },
-              null,
-              { label: 'Upload File', action: handleUploadClick },
-              { label: 'Download Folder', action: handleDownloadClick },
-            ]}
-          >
-            <ScrollArea
-              ref={dropzoneRef}
-              className={cn(
-                'relative h-[calc(100%_-_45px)] w-full',
-                isOver && 'bg-border',
-              )}
-              onClick={handleClick}
-            >
-              <ExplorerFolderChildren folder={explorer} />
+      <div className="h-[calc(100%_-_40px)]">
+        {args && <ExplorerFileItem file={args} meta />}
+        {input && <ExplorerFileItem file={input} meta />}
+        <Separator />
 
-              {/* Take into account the meta files (2) */}
-              {explorer.children.length <= 2 && (
-                <div className="absolute inset-0 flex items-center justify-center">
-                  <span className="max-w-64 text-balance p-1 text-center text-foreground/50 text-sm">
-                    This is the file explorer. Create a new file to get started.
-                  </span>
-                </div>
-              )}
-            </ScrollArea>
-          </ContextMenuWrapper>
-        </div>
-      </section>
-    </ContextMenuWrapper>
+        <ScrollArea
+          ref={dropzoneRef}
+          className={cn(
+            'relative h-[calc(100%_-_45px)] w-full',
+            isOver && 'bg-border',
+          )}
+          onClick={handleClick}
+        >
+          <ExplorerFolderChildren folder={explorer} />
+
+          {/* Take into account the meta files (2) */}
+          {explorer.children.length <= 2 && (
+            <div className="absolute inset-0 flex items-center justify-center">
+              <span className="max-w-64 text-balance p-1 text-center text-foreground/50 text-sm">
+                This is the file explorer. Create a new file to get started.
+              </span>
+            </div>
+          )}
+        </ScrollArea>
+      </div>
+    </section>
   );
 }

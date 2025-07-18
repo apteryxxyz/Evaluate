a1184c15da84266e0f1c0cf6c833b07544e84078
Apteryx <apteryx.xyz@gmail.com>
2025-07-18 12:37:08 +1200

    Fix missing terminal styling

--- apps/website/src/components/terminal/index.tsx
+++ apps/website/src/components/terminal/index.tsx
@@ -87,14 +87,14 @@ export function Terminal({ runtime }: Terminal.Props) {
           {result?.run.output && <ReadonlyEditor content={result.run.output} />}
 
           {!result?.run.output && (
-            <div>
-              <span>
+            <div className="absolute inset-0 flex items-center justify-center bg-black/40">
+              <span className="max-w-64 text-balance text-center text-foreground/50 text-sm">
                 {result?.run.expired ? (
                   <>
                     Your code execution exceeded the allotted time and was
                     terminated. Consider optimising it for better performance.
                   </>
-                ) : !result?.run.output ? (
+                ) : result?.run.output?.trim() === '' ? (
                   <>
                     Your code executed successfully; however, it did not
                     generate any output for the console.
@@ -117,8 +117,8 @@ export function Terminal({ runtime }: Terminal.Props) {
           )}
 
           {!result?.compile?.output && (
-            <div>
-              <span>
+            <div className="absolute inset-0 flex items-center justify-center bg-black/40">
+              <span className="max-w-64 text-balance text-center text-foreground/50 text-sm">
                 {result?.compile?.expired ? (
                   <>
                     Your code compilation exceeded the allotted time and was
@@ -141,143 +141,7 @@ export function Terminal({ runtime }: Terminal.Props) {
             <HtmlPreview>{result?.run?.output}</HtmlPreview>
           )}
 
-          {!(runtime.id === 'php') && <div></div>}
-        </TabsContent>
-      </Tabs>
-    </section>
-  );
-
-  /*
-  const { result, setResult } = useTerminal();
-
-  const hasRun = result && 'run' in result;
-  const hasRunTimedOut = hasRun && result.run?.signal === 'SIGKILL';
-  const doesRunHaveDisplayableOutput =
-    hasRun && result?.run?.output?.trim() !== '';
-  const doesRunHaveAnEmptyOutput =
-    !doesRunHaveDisplayableOutput && result?.run?.output !== undefined;
-
-  const hasCompile = result && 'compile' in result;
-  const hasCompileTimedOut = hasCompile && result.compile?.signal === 'SIGKILL';
-  const doesCompileHaveDisplayableOutput =
-    hasCompile && result?.compile?.output?.trim() !== '';
-
-  const hasPreview = runtime.id === 'php';
-
-  return (
-    <section className="h-full">
-      <Tabs defaultValue="run" className="h-full gap-0">
-        <ScrollArea className="flex items-center whitespace-nowrap rounded-full">
-          <TabsList className="h-10 w-full space-x-1 rounded-none border-b-2 bg-transparent px-0.5">
-            {(
-              [
-                [
-                  CodeIcon, //
-                  'run',
-                  'Run',
-                  doesRunHaveDisplayableOutput,
-                ],
-                [
-                  PackageCheckIcon,
-                  'compile',
-                  'Compile',
-                  doesCompileHaveDisplayableOutput,
-                ],
-                [
-                  FullscreenIcon,
-                  'preview',
-                  'Preview',
-                  hasPreview && doesRunHaveDisplayableOutput,
-                ],
-              ] as const
-            ).map(([Icon, value, label, indicator]) => (
-              <TabsTrigger key={label} value={value} asChild>
-                <Button
-                  variant="secondary"
-                  className="relative bg-card text-foreground/70 hover:text-foreground data-[state=active]:bg-muted data-[state=active]:text-foreground"
-                >
-                  <Icon className="size-4" />
-                  <span>&nbsp;{label}</span>
-                  {indicator && (
-                    <span className="-mt-1 -mr-1 absolute top-2 right-2 size-1.5 rounded-full bg-primary/50" />
-                  )}
-                </Button>
-              </TabsTrigger>
-            ))}
-
-            <Button
-              variant="secondary"
-              size="icon"
-              onClick={() => setResult(undefined)}
-              className="ml-auto bg-card text-foreground/70 hover:text-foreground"
-              disabled={!result}
-            >
-              <Trash2Icon className="size-4" />
-              <span className="sr-only">Clear</span>
-            </Button>
-          </TabsList>
-          <ScrollBar orientation="horizontal" />
-        </ScrollArea>
-
-        <TabsContent value="run" className="relative mt-0 h-full">
-          {doesRunHaveDisplayableOutput && (
-            <ReadonlyEditor content={result.run.output} />
-          )}
-
-          {!doesRunHaveDisplayableOutput && (
-            <div className="absolute inset-0 flex items-center justify-center bg-black/40">
-              <span className="max-w-64 text-balance text-center text-foreground/50 text-sm">
-                {hasRunTimedOut ? (
-                  <>
-                    Your code execution exceeded the allotted time and was
-                    terminated. Consider optimising it for better performance.
-                  </>
-                ) : doesRunHaveAnEmptyOutput ? (
-                  <>
-                    Your code executed successfully; however, it did not
-                    generate any output for the console.
-                  </>
-                ) : (
-                  <>
-                    Write your code and press{' '}
-                    <PlayIcon size={16} className="inline" /> to execute it. The
-                    results will be displayed here.
-                  </>
-                )}
-              </span>
-            </div>
-          )}
-        </TabsContent>
-
-        <TabsContent value="compile" className="relative mt-0 h-full">
-          {doesCompileHaveDisplayableOutput && (
-            <ReadonlyEditor content={result.compile?.output ?? ''} />
-          )}
-
-          {!doesCompileHaveDisplayableOutput && (
-            <div className="absolute inset-0 flex items-center justify-center bg-black/40">
-              <span className="max-w-64 text-balance text-center text-foreground/50 text-sm">
-                {hasCompileTimedOut ? (
-                  <>
-                    Your code compilation exceeded the allotted time and was
-                    terminated. Consider optimizing your code for faster
-                    compilation.
-                  </>
-                ) : (
-                  <>
-                    <strong>If</strong> this runtime requires compilation, the
-                    output will be displayed here.
-                  </>
-                )}
-              </span>
-            </div>
-          )}
-        </TabsContent>
-
-        <TabsContent value="preview" className="relative mt-0 h-full">
-          {hasPreview && <HtmlPreview>{result?.run?.output}</HtmlPreview>}
-
-          {!hasPreview && (
+          {!(runtime.id === 'php') && (
             <div className="absolute inset-0 flex items-center justify-center bg-black/40">
               <span className="max-w-64 text-balance text-center text-foreground/50 text-sm">
                 This runtime does not have a way to display any preview.
@@ -288,5 +152,4 @@ export function Terminal({ runtime }: Terminal.Props) {
       </Tabs>
     </section>
   );
-  */
 }

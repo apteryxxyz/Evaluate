dcbb25abd11751c55a06fcdffe895999a55b484c
Apteryx <apteryx.xyz@gmail.com>
2025-10-18 12:54:50 +1300

    Remove testing translations

--- apps/discord-bot/src/i18n.ts
+++ apps/discord-bot/src/i18n.ts
@@ -4,9 +4,6 @@ const say = new Sayable({
   en: () =>
     import('./locales/en/messages.json', { with: { type: 'json' } }) //
       .then((m) => m.default),
-  fr: () =>
-    import('./locales/fr/messages.json', { with: { type: 'json' } }) //
-      .then((m) => m.default),
 });
 
 await say.load();
